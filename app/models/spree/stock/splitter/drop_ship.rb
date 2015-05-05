1ee898e6e74ab6bc7755dbb813f58703cd5973f8
Ahmed Mohsen <ahmed7890@gmail.com>
2015-05-05 16:29:19 +0200

    [FIX] passing variant as param

--- app/models/spree/stock/splitter/drop_ship.rb
+++ app/models/spree/stock/splitter/drop_ship.rb
@@ -12,10 +12,12 @@ module Spree
             # Determine which supplier to package drop shipped items.
             drop_ship = package.contents.select { |content| content.variant.suppliers.count > 0 }
             drop_ship.each do |content|
+              # Select the related variant
+              variant = content.variant
               # Select suppliers ordering ascending according to cost.
-              suppliers = content.variant.supplier_variants.order('spree_supplier_variants.cost ASC').map(&:supplier)
+              suppliers = variant.supplier_variants.order('spree_supplier_variants.cost ASC').map(&:supplier)
               # Select first supplier that has stock location with avialable stock item.
-              available_supplier = suppliers.detect { |supplier| supplier.stock_locations_with_available_stock_items.any? }
+              available_supplier = suppliers.detect { |supplier| supplier.stock_locations_with_available_stock_items(variant).any? }
               # Select the first available stock location with in the available_supplier stock locations.
               stock_location = available_supplier.stock_locations_with_available_stock_items.first
               # Add to any existing packages or create a new one.
